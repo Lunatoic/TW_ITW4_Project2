0f7c016c90bf58ca91eabdc35d65230fd1e36b57
Nikhil <nikhildgr788@gmail.com>
2022-07-09 23:42:29 +0530

    fitness section updating

---
+++ Fitness/js/fitness.js
@@ -0,0 +1,6 @@
+let header = document.querySelector('.header');
+let headerScrolled = document.querySelector('.headerScrolled');
+
+document.addEventListener('scroll', ()=> {
+        // header.classList.add("headerScrolled");
+});
